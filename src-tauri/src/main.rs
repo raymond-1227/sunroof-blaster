ff1180187f287f95ef5ca0783ddca47287db6ad7
Raymond Hsu <raymondhsu@duck.com>
2024-07-24 18:25:32 +0800

    bug fixes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 fn main() {
     tauri::Builder::default()
